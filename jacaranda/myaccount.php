5b8b83021f637dbfa960646d062dfa1bc1d4e725
KevinEsco <kevinescobedo248@gmail.com>
2019-02-11 18:03:18 -0300

    correcciones menores, error falta email en sesion.

--- jacaranda/myaccount.php
+++ jacaranda/myaccount.php
@@ -4,79 +4,189 @@ session_start();
 ?>
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://fonts.googleapis.com/css?family=Charmonman|Leckerli+One" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
-	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
-    
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
-    <link rel="stylesheet" href="main.css">
-    <title>Mi cuenta</title>
-</head>
+  <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width, initial-scale=1.0">
+      <meta http-equiv="X-UA-Compatible" content="ie=edge">
+      <link href="https://fonts.googleapis.com/css?family=Charmonman|Leckerli+One" rel="stylesheet">
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.3.1/css/all.css" integrity="sha384-mzrmE5qonljUremFsqc01SB46JvROS7bZs3IO2EmfFsd15uHvIt+Y8vEf7N7fWAU" crossorigin="anonymous">
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.min.js"/>
+      <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script> 
+      <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css" integrity="sha384-WskhaSGFgHYWDcbwN70/dfYBj47jz9qbsMId/iRN3ewGhXQFZCSftd1LZCfmhktB" crossorigin="anonymous">
+      <link rel="stylesheet" href="main.css">
+      <title> Mi cuenta </title>
+  </head>
 <body>
 <!--NavBar -->
-    <nav class="navbar  navbar-expand-lg navbar-light bg-light, nav justify-content-center">
-                        <ul class="nav justify-content-center">
-                              <li class="nav-item">
-                                <a  href="index.php"><img class="logo"src="logo.jpg" alt=""></a>
-                              </li>
-                              
-                             
-                              
-                                        
-                              <li class="nav-item nologo">
-                                <a class="nav-link" href="#">Conocenos</a>
-                              </li>
-        
-        
-                              <li class="nav-item nologo">
-                                <a class="nav-link " href="#">Contacto</a>
-                              </li>
-        
-                              <li class="nav-item nologo">
-                                <a class="nav-link " href="#">Novedades</a>
-                              </li>
-                              
-                                 <a  ><i onclick="myFunction()" class="fas fa-shopping-bag b icono"></i> </a>
-                              </li>
-                              </li>
-                              
-                                 <a href="login.php"><i class="fas fa-user b icono"></i></a> 
-                              </li>
-                              <!-- <div class="busqueda nologo">
-                                          
-                                  <div class="input-group stylish-input-group">
-                                      <input type="text" class="form-control"  placeholder="Search" >
-                                      <span class="input-group-addon">
-                                          <button class="btn-outline-primary btn" type="submit">
-                                              <span class="fas fa-search"></span>
-                                          </button>  
-                                      </span>
-                                  </div>
-                              
-                        
-                              </div> -->
-                              <div id="container icono">
-                                <form role="search" method="get" id="searchform" action="">
-                                  <label for="s">
-                                    <i class="fas fa-search"></i>
-                                  </label>
-                                  <input type="text" value="" placeholder="search" class="" id="s" />
-                                </form>
-                                </div>
-                        </ul>
-                      </nav>
+  <nav class="navbar navbar-expand-lg navbar-light bg-light, nav justify-content-center">
+    <ul class="nav justify-content-center">
+          <li class="nav-item">
+            <a  href="index.php"><img class="logo"src="logo.jpg" alt=""></a>
+          </li>
+          
+              
+          <li class="nav-item nologo">
+            <a class="nav-link" href="#">Conocenos</a>
+          </li>
+
+          <li class="nav-item nologo">
+            <a class="nav-link " href="#">Contacto</a>
+          </li>
+
+          <li class="nav-item nologo">
+            <a class="nav-link " href="#">Novedades</a>
+          </li>
+            <a><i class="fas fa-shopping-bag b icono"  data-toggle="modal" data-target="#exampleModal"></i> </a>
+          </li>
+          </li>
+          
+              <a href="login.php"><i class="fas fa-user b icono"></i></a> 
+          </li>
+          <!-- <div class="busqueda nologo">
+                      
+              <div class="input-group stylish-input-group">
+                  <input type="text" class="form-control"  placeholder="Search" >
+                  <span class="input-group-addon">
+                      <button class="btn-outline-primary btn" type="submit">
+                          <span class="fas fa-search"></span>
+                      </button>  
+                  </span>
+              </div>
+          
+    
+          </div> -->
+          <div id="container icono">
+            <form role="search" method="get" id="searchform" action="">
+              <label for="s">
+                <i class="fas fa-search"></i>
+              </label>
+              <input type="text" value="" placeholder="search" class="" id="s" />
+            </form>
+            </div>
+    </ul>
+  </nav>
+  <!-- Modal Cart-->
+<div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+
+        <div class="modal-dialog modal-lg">
+
+                <div class="modal-content">
+                
+    <?php
+    if(isset($_SESSION["cart_item"])){
+        $total_quantity = 0;
+        $total_price = 0;
+    ?>	
+
+                <table class="tbl-cart" cellpadding="10" cellspacing="1">
+                    <tbody>
+                            <tr>
+                                    <th style="text-align:left;">Nombre</th>
+                                    <th style="text-align:left;">Cod.</th>
+                                    <th style="text-align:right;" width="5%">Cantidad</th>
+                                    <th style="text-align:right;" width="10%">Precio Unitario</th>
+                                    <th style="text-align:right;" width="10%">Total</th>
+                                    <th style="text-align:right;" width="5%">Talle</th>
+                                    <th style="text-align:center;" width="5%">Eliminar</th>
+                            </tr>	
+    <?php		
+    foreach ($_SESSION["cart_item"] as $item) {
+
+        $item_price = $item["quantity"]*$item["price"];
+    ?>
+                            <tr>
+                                    <td>
+                                    <img src="<?php echo $item["image"]; ?>" class="cart-item-image" />
+    <?php echo $item["name"]; ?>
+                                    </td>
+                                    <td>
+    <?php echo $item["code"]; ?>
+                                    </td>
+
+                                    <td style="text-align:right;">
+    <?php echo $item["quantity"]; ?>
+                                    </td>
+
+                                    <td  style="text-align:right;">
+    <?php echo "$ ".$item["price"]; ?>
+                                    </td>
+
+                                    <td  style="text-align:right;">
+    <?php echo "$ ". number_format($item_price,2); ?>
+                                    </td>
+                                    
+                                    <td style="text-align:right;">
+    <?php echo $item["talle"];   ?>
+                                    </td>
+                                    <td style="text-align:center;">
+                                        <a href="index.php?action=remove&code=<?php echo $item["code"]; ?>"
+                                           class="btnRemoveAction"
+                                        >
+                                            <i class="fas fa-trash"></i>
+                                        </a>
+                                    </td>
+                                
+                            </tr>
+    <?php
+        $total_quantity += $item["quantity"];
+        $total_price += ($item["price"]*$item["quantity"]);
+        }
+    ?>
+
+                            <tr>
+                            
+                                    <td colspan="2" align="left">
+
+                                        <button class="btn btn-danger btn-carro">
+
+                                        <a id="btnEmpty" href="index.php?action=empty">
+                                            Vaciar Carro
+                                        </a> 
+
+                                        <button class="btn btn-danger btn-carro">
+
+                                        <a id="btnEmpty" href="checkout.php">
+                                            Continuar compra
+                                        </a>
+
+                                    </td>
+                                            
+                                    <td colspan="2" align="right">Total:</td>
+                                    <td align="right">
+    <?php echo $total_quantity; ?> 
+                                    </td>
+                                    
+                                    <td align="right" colspan="4">
+                                        <strong>
+    <?php echo "$ ".number_format($total_price, 2); ?>
+                                        </strong>
+                                    </td>
+                                    
+                            </tr>
+                    </tbody>
+                </table>		
+    <?php
+    } else {
+    ?>
+
+                <div class="no-records">
+                    Tu carro esta vacio
+                </div>
+                <style> #btnEmpty{visible:false}</style>
+    
+    <?php 
+    }
+    ?>
+                </div> 
+        </div>
+</div>
     <div class="container">
-   <h4> Informacion de mi cuenta</h4><br>
-   Mail: <?php echo htmlspecialchars($_SESSION["username"]); ?>
+   <h2> Informacion de mi cuenta</h2><br>
+   Mail: <?php echo htmlspecialchars($_SESSION["email"]); ?>
    <p>
         <a href="reset-password.php" class="btn btn-warning">Reestablecer contraseña</a>
-        <a href="logout.php" class="btn btn-danger">Sign Out of Your Account</a>
+        <a href="logout.php" class="btn btn-danger"> Cerrar sesion </a>
     </p>
     </div>
     
